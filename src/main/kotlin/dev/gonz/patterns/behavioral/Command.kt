b4606d8c0fdacb9ccb5beb3aa8e0ef1b18ed17f6
Gonzalo Campos <gonzalocampose1@gmail.com>
2023-02-05 18:07:16 -0600

    Add code for Command design pattern.

--- src/main/kotlin/dev/gonz/patterns/behavioral/Command.kt
+++ src/main/kotlin/dev/gonz/patterns/behavioral/Command.kt
@@ -1,1 +1,71 @@
 package dev.gonz.patterns.behavioral
+
+interface Document {
+    val fileName: String
+
+    fun open(fileName: String)
+    fun save(newFileName: String)
+    fun close()
+    fun format()
+    fun changeBackgroundColor()
+}
+
+interface Command {
+    fun execute()
+}
+
+class MacroCommand : Command {
+    private val commands = mutableListOf<Command>()
+
+    override fun execute() {
+        commands.forEach(Command::execute)
+    }
+
+    fun add(command: Command) {
+        commands.add(command)
+    }
+
+    fun remove(command: Command) {
+        commands.remove(command)
+    }
+}
+
+class OpenDocumentCommand(
+    val document: Document,
+) : Command {
+    override fun execute() {
+        val fileName = askUser()
+        document.open(fileName)
+    }
+
+    private fun askUser(): String {
+        return "my_awesome_file.txt"
+    }
+}
+
+class SaveDocumentCommand(
+    val document: Document,
+    val newFileName: String? = null,
+) : Command {
+    override fun execute() {
+        document.save(newFileName ?: document.fileName)
+    }
+}
+
+class CloseDocumentCommand(
+    val document: Document,
+) : Command {
+    override fun execute() {
+        document.close()
+    }
+}
+
+class SimpleCommand<T>(
+    val receiver: T,
+    val action: T.() -> Unit,
+) : Command {
+
+    override fun execute() {
+        receiver.action()
+    }
+}
